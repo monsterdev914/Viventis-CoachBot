cb9b0aa29433a1edee71d421f6332f65f71a8533
SeniorDev2510 <jovezaaca@gmail.com>
2025-06-25 03:14:25 -0700

    fix: logo change

--- front-end/components/navbar.tsx
+++ front-end/components/navbar.tsx
@@ -48,7 +48,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="#">
             <Logo />
-            <p className="font-bold text-inherit">{t("Viventis")}</p>
+            <p className="font-bold text-inherit">{t("Viventis Bot")}</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
